3f354ed8e71d8c8ad7b4b52bbacbc30d9af38a04
maple <datongde@gmail.com>
2017-01-08 00:13:32 +0800

    implement step 5

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,6 +5,10 @@ class JobsController < ApplicationController
   end
   def show
     @job = Job.find(params[:id])
+    if @job.is_hidden
+      flash[:warning] = "This Job already archieved"
+      redirect_to root_path
+    end
   end
   def new
     @job = Job.new
@@ -34,6 +38,7 @@ class JobsController < ApplicationController
     @job.destroy
     redirect_to jobs_path
   end
+  
 
   private
 
--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,2 +1,10 @@
 module JobsHelper
+  def render_job_status(job)
+    if job.is_hidden
+       content_tag(:span, "", :class => "fa fa-lock")
+      # content_tag(:span, "", :class => "fa fa-window-close")
+    else
+      content_tag(:span, "", :class => "fa fa-globe")
+    end
+  end
 end
